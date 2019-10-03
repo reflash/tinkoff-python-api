891a95125ca1be9f87edc011564c8e24dba222cf
reflash <reflash-ne@yandex.ru>
2019-10-03 11:21:48 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='Tinkoff Invest Python API',
+    version='0.1dev',
+    packages=['client',],
+    long_description="Generated client for Tinkoff Invest API",
+)
